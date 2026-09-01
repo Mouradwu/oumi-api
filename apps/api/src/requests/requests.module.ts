970b16df76fc237a455a635940c03fcf955a4aef
Mourad <mourad@example.com>
2026-09-01 10:41:35 +0200

    fix: add User repository to RequestsModule

--- apps/api/src/requests/requests.module.ts
+++ apps/api/src/requests/requests.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RequestsController } from './requests.controller';
 import { RequestsService } from './requests.service';
-import { DonationRequest } from './entities/request.entity';
+import { DonationRequest } from './donation-request.entity';
+import { User } from '../users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DonationRequest])],
+  imports: [TypeOrmModule.forFeature([DonationRequest, User])],
   controllers: [RequestsController],
   providers: [RequestsService],
-  exports: [RequestsService],
 })
 export class RequestsModule {}
